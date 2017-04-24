c96a5ce102257d0bedd8f3a3289ee42e5f498776
Vishal Rastogi <vishal.rastogi13@yahoo.com>
2017-04-24 17:50:41 +0000

    initialising code

--- recover/recover.c
+++ recover/recover.c
@@ -22,6 +22,27 @@ int main(int argc, char *argv[])
         printf("Could not open card.raw.\n");
         return 2;
     }
+    
+    // create buffer
+    unsigned char buffer[512];
+    
+    // filename counter
+    int filecount = 0;
+    
+    FILE* picture = NULL; 
+    
+    // check if we've found a jpeg yet or not
+    int jpg_found = 0; //false
+    
+    // go through cardfile until there aren't any blocks left
+    while (fread(buffer, 512, 1, input) == 1)
+    {
+        // read first 4 bytes of buffer and see if jpg signature using bitwise on last byte
+       
+        
+        
+        
+    }
 
     
 
